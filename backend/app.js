0b38d914fd6d05357c04fe04915f8f381a3ae6d3
Saurav Raj <sanraj4356@gmail.com>
2025-07-24 17:28:36 +0530

    Resolve app

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 require("./conn/conn");
+const serverless = require("serverless-http");
 const cors = require("cors");
 const auth = require("./routes/auth");
 const list = require("./routes/list");
 const path = require("path");
 app.use(express.json());
 app.use(cors());
-app.get("/", (req, res) => {
+
+app.get("/ping", (req, res) => {
   res.send("Backend is running!");
 });
 
@@ -15,4 +17,4 @@ app.get("/", (req, res) => {
 app.use("/api/v1",auth);
 app.use("/api/v2",list);
 
-module.exports = app;
+module.exports.handler = serverless(app);
