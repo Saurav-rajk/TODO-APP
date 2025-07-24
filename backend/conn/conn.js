31f461027d7d421769f20c2f60e9a6bf831d49d5
Saurav Raj <sanraj4356@gmail.com>
2025-07-24 17:04:06 +0530

    Resolve j

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const conn = async (req,res)=>{
+const conn = async () => {
   try {
-    await mongoose.connect("mongodb+srv://user:saurav123@cluster0.hu60fo7.mongodb.net/").then(()=>{
-    console.log("Connected");
-    
-  });
-  } catch (error) {
-    res.status(400).json({message: "Not Connected"});
+    const connection = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB connected: ${connection.connection.host}`);
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
   }
 };
-conn();
+
+module.exports = conn;
+
